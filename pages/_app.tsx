6b666677223f60b9a60749b9c056f1536d5009e0
maddhruv <dhruvjainpenny@gmail.com>
2020-05-26 01:13:51 +0530

    ci: add eslint, prettier

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,8 @@
-import React from "react";
-import Head from "next/head";
-import { AppProps } from "next/app";
+import React from 'react';
+import Head from 'next/head';
+import { AppProps } from 'next/app';
 
-export default ({ Component, pageProps }: AppProps) => {
+const App = ({ Component, pageProps }: AppProps) => {
   return (
     <>
       <Head>
@@ -12,3 +12,5 @@ export default ({ Component, pageProps }: AppProps) => {
     </>
   );
 };
+
+export default App;
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,16 +1,14 @@
-import Document, { Html, Head, Main, NextScript } from "next/document";
+import React from 'react';
+import Document, { Html, Head, Main, NextScript } from 'next/document';
 
 export default class MyDocument extends Document {
   render() {
     return (
-      <Html lang="en">
+      <Html lang='en'>
         <Head>
-          <link
-            href="https://fonts.googleapis.com/css?family=Work+Sans&display=swap"
-            rel="stylesheet"
-          />
-          <link rel="icon" type="image/png" href="/logo-16.png" />
-          <link rel="manifest" href="/manifest.json" />
+          <link href='https://fonts.googleapis.com/css?family=Work+Sans&display=swap' rel='stylesheet' />
+          <link rel='icon' type='image/png' href='/logo-16.png' />
+          <link rel='manifest' href='/manifest.json' />
         </Head>
         <body>
           <Main />
