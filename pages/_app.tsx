a5b26989e2bb71800e00dd0eb2d1a961d87d97e3
maddhruv <dhruvjainpenny@gmail.com>
2020-05-28 20:28:29 +0530

    feat: progress bar

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,19 @@
 import Head from 'next/head';
 import { AppProps } from 'next/app';
+import Router from 'next/router';
 import { ThemeProvider } from 'styled-components';
+import Progress from 'nprogress';
+
+import '../css/nprogress.css';
 
 import theme from '../configs/theme';
 import GlobalStyles from '../components/GlobalStyles';
 import TitleBar from '../components/TitleBar';
 import PlaygroundStore from '../stores/playground';
+
+Router.events.on('routeChangeStart', () => Progress.start());
+Router.events.on('routeChangeComplete', () => Progress.done());
+Router.events.on('routeChangeError', () => Progress.done());
 
 const App = ({ Component, pageProps }: AppProps) => {
   return (
