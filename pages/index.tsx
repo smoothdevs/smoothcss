6e8b4f5f9c40c02276c83589b26bdbe50b58640e
maddhruv <dhruvjainpenny@gmail.com>
2020-06-06 09:25:01 +0530

    revert: redirection

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,11 @@
 import React from 'react';
-import { NextPage, NextPageContext } from 'next';
 import Link from 'next/link';
 import { Flex, Box } from 'reflexbox';
 
 import Card from '@components/Card';
 import blocks from '@blocks/index';
 
-const Home: NextPage = () => (
+const Home: React.FC = () => (
   <Flex p={['5px', '30px']} flexDirection={['column', 'row']} flexWrap='wrap' textAlign='center'>
     {Object.entries(blocks).map((block) => (
       <Link href='/components/[component]' as={`/components/${block[0]}`} key={block[0]}>
@@ -17,21 +16,5 @@ const Home: NextPage = () => (
     ))}
   </Flex>
 );
-
-Home.getInitialProps = (ctx: NextPageContext) => {
-  const allBlocks = Object.keys(blocks);
-
-  const component = allBlocks[Math.floor(Math.random() * allBlocks.length)];
-
-  const { res } = ctx;
-  if (res) {
-    res.writeHead(301, {
-      Location: `/components/${component}`,
-    });
-
-    res.end();
-  }
-  return {};
-};
 
 export default Home;
