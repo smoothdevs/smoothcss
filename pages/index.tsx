c3ebca7d946c2daff660c15895cd7b1b1f89b44f
maddhruv <dhruvjainpenny@gmail.com>
2020-06-06 09:15:25 +0530

    feat: redirect from index page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,22 +1,37 @@
 import React from 'react';
+import { NextPage, NextPageContext } from 'next';
 import Link from 'next/link';
 import { Flex, Box } from 'reflexbox';
 
 import Card from '@components/Card';
 import blocks from '@blocks/index';
 
-const Home: React.FC = () => {
-  return (
-    <Flex p={['5px', '30px']} flexDirection={['column', 'row']} flexWrap='wrap' textAlign='center'>
-      {Object.entries(blocks).map((block) => (
-        <Link href='/components/[component]' as={`/components/${block[0]}`} key={block[0]}>
-          <Box width={[1, 1 / 3]} p={['10px']}>
-            <Card>{block[1].name}</Card>
-          </Box>
-        </Link>
-      ))}
-    </Flex>
-  );
+const Home: NextPage = () => (
+  <Flex p={['5px', '30px']} flexDirection={['column', 'row']} flexWrap='wrap' textAlign='center'>
+    {Object.entries(blocks).map((block) => (
+      <Link href='/components/[component]' as={`/components/${block[0]}`} key={block[0]}>
+        <Box width={[1, 1 / 3]} p={['10px']}>
+          <Card>{block[1].name}</Card>
+        </Box>
+      </Link>
+    ))}
+  </Flex>
+);
+
+Home.getInitialProps = (ctx: NextPageContext) => {
+  const allBlocks = Object.keys(blocks);
+
+  const component = allBlocks[Math.floor(Math.random() * allBlocks.length)];
+
+  const { res } = ctx;
+  if (res) {
+    res.writeHead(301, {
+      Location: `/components/${component}`,
+    });
+
+    res.end();
+  }
+  return {};
 };
 
 export default Home;
